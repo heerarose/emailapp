12d3583ba80a53199f9ebcea22b53a54f21cd5ff
heerarose <roseheeraa@gmail.com>
2024-08-30 15:44:08 +0530

    emailapp scanner

--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -1,8 +1,15 @@
 package emailapp;
+import java.util.Scanner;
 
 public class EmailApp {
+
     public static void main(String[] args) {
-        Email em1 = new Email("Heera Rose", "Benet");
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter the first name:");
+        String firstName = sc.nextLine();
+        System.out.print("Enter the last name:");
+        String lastName = sc.nextLine();
+        Email em1 = new Email(firstName, lastName);
         em1.setAlternateEmail("heerarose@gmail.com");
         System.out.println(em1.getAlternateEmail());
         System.out.println(em1.showInfo());
